7ea9ff184f9a5049d7b7db4b2422e9120d127e83
Sapov <sapov@mail.ru>
2024-08-07 23:08:18 +0300

    add urls

---
+++ cinemas/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import CinemasListView, CinemasDetail, CinemasDetailAPIView
+
+app_name = 'Cinemas'
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('list/', CinemasListView.as_view(), name='cinemalist'),
+    path('list/<int:pk>/', CinemasDetail.as_view(), name='cinema'),
+    path('listdetail/<int:pk>/', CinemasDetailAPIView.as_view(), name='cinema_detail'),
+
+]
