a7844140bd4dd91804b1db1717efde8a18f2d8ab
Sapov <sapov@mail.ru>
2024-08-07 23:07:19 +0300

    add serializers.py

---
+++ cinemas/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from cinemas.models import CinemaGeneral
+
+
+class CinemaGeneralSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaGeneral
+        fields = ['name', 'description', 'street']
